0aa4e729be94fb4ca78a46b55ed4856d30483892
Ralph Lange <Ralph.Lange@bessy.de>
2002-07-19 06:29:51 +0000

    1.3.3 - Bugfix: double call to regexp match in pvExistTest() fixed.

--- gateVersion.h
+++ gateVersion.h
@@ -1,19 +1,33 @@
-// gateVersion.h:  Gateway Version Information
+#ifndef _GATEVERSION_H_
+#define _GATEVERSION_H_
 
-///////////////////////////////////////////////////////////
-// Version History Log
-///////////////////////////////////////////////////////////
-// 1.1.0 02-08-99  evans
-///////////////////////////////////////////////////////////
-
+/*+*********************************************************************
+ *
+ * File:       gateVersion.h
+ * Project:    CA Proxy Gateway
+ *
+ * Descr.:     Gateway Version Information
+ *
+ * Author(s):  J. Kowalkowski, J. Anderson, K. Evans (APS)
+ *             R. Lange (BESSY)
+ *
+ * $Revision$
+ * $Date$
+ *
+ * $Author$
+ *
+ * $Log$
+ *********************************************************************-*/
 
 #define GATEWAY_VERSION       1
 #define GATEWAY_REVISION      3
-#define GATEWAY_MODIFICATION  2
+#define GATEWAY_MODIFICATION  3
 
-#define GATEWAY_VERSION_STRING	"CA Proxy Gateway Version 1.3.2"
+#define GATEWAY_VERSION_STRING "CA Proxy Gateway Version 1.3.3"
 
 #define GATEWAY_CREDITS_STRING  \
           "Developed at Argonne National Laboratory and BESSY\n\n" \
           "Authors: Jim Kowalkowski, Janet Anderson, Kenneth Evans, Jr. (APS)\n" \
           "         Ralph Lange (BESSY)\n\n"
+
+#endif
