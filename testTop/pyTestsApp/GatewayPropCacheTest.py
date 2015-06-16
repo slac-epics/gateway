743fd31a309eed742f16dd5017b959c6fe1517b9
Ralph Lange <ralph.lange@gmx.de>
2015-06-16 14:13:18 +0200

    test: GatewayPropCache speed up

--- testTop/pyTestsApp/GatewayPropCacheTest.py
+++ testTop/pyTestsApp/GatewayPropCacheTest.py
@@ -18,23 +18,17 @@ class GatewayPropCacheTest(unittest.TestCase):
         self.gatewayControl = GatewayControl.GatewayControl()
         self.siocControl.startSIOCWithDefaultDB("12782")
         self.gatewayControl.startGateway(os.environ['EPICS_CA_SERVER_PORT'] if 'EPICS_CA_SERVER_PORT' in os.environ else "5064", "12782")
-        time.sleep(2)
         epics.ca.initialize_libca()
 
-        
     def tearDown(self):
-        time.sleep(1)
         epics.ca.finalize_libca()
         self.siocControl.stop()
-        time.sleep(1)
         self.gatewayControl.stop()
         
     def testGatewayPropCache(self):
         '''Establish no monitors - make a change to the HIGH outside of the gateway; then ca_get the value of the PV's HIGH from the gateway; received changed value'''
         pvHIGH = epics.PV("gateway:gwcachetest.HIGH", auto_monitor=None)
-        time.sleep(1)
         highVal = pvHIGH.get()
-        time.sleep(1)
         self.assertTrue(highVal == 10.0, "We expect the HIGH to be 10. Instead it is " + str(highVal))
         # make a change to the HIGH outside the gateway; so we talk to the IOC directly. 
         if gwtests.verbose:
@@ -47,12 +41,8 @@ class GatewayPropCacheTest(unittest.TestCase):
             DEVNULL = open(os.devnull, 'wb')
         for val in range(10, 100, 10):
             subprocess.Popen(['caput', 'gateway:gwcachetest.HIGH', str(val)], env=caputEnv, stdout=DEVNULL, stderr=subprocess.STDOUT)
-            time.sleep(1)
             highVal = pvHIGH.get()
-            time.sleep(1)
             self.assertTrue(highVal == val, "We expect the HIGH to be " + str(val) + " Instead it is " + str(highVal))
 
         if DEVNULL:
             DEVNULL.close()
-        
-        
