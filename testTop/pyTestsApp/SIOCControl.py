e8fc840708c36d002f82b09d8d90368a65faf3e1
Ralph Lange <ralph.lange@gmx.de>
2015-06-22 09:27:03 +0200

    test: enhance SIOCControl to use EPICS_BASE for starting IOC

--- testTop/pyTestsApp/SIOCControl.py
+++ testTop/pyTestsApp/SIOCControl.py
@@ -17,7 +17,18 @@ class SIOCControl:
         childEnviron['EPICS_CA_AUTO_ADDR_LIST'] = "NO"
         if not gwtests.verbose:
             self.DEVNULL = open(os.devnull, 'wb')
-        self.siocProcess = subprocess.Popen(['softIoc', '-d', 'test.db'], env=childEnviron, stdout=self.DEVNULL, stderr=subprocess.STDOUT)
+
+        siocExe = 'softIoc'
+        if 'EPICS_BASE' in os.environ:
+            if 'EPICS_HOST_ARCH' not in os.environ:
+                print "Please set the EPICS_HOST_ARCH environment variable to the appropriate value."
+                sys.exit(1)
+            siocExe = "{0}/bin/{1}/softIoc".format(os.environ['EPICS_BASE'],os.environ['EPICS_HOST_ARCH'])
+        siocCommand = [siocExe, '-d', 'test.db']
+
+        if gwtests.verbose:
+            print "Starting the IOC using\n", " ".join(siocCommand)
+        self.siocProcess = subprocess.Popen(siocCommand, env=childEnviron, stdout=self.DEVNULL, stderr=subprocess.STDOUT)
         time.sleep(.5)
 
     def stop(self):
