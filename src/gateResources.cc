c0f848951b2a8fce817644352c94c3045fb6936d
Freddie Akeroyd <freddie.akeroyd@stfc.ac.uk>
2019-12-17 12:43:40 +0000

    Always print unsigned char

--- src/gateResources.cc
+++ src/gateResources.cc
@@ -195,11 +195,12 @@ static int gddToVALUE(const gdd *gddVal, short dbfld_dbrtype, VALUE *valueStruct
  */
 static int VALUE_to_string(char *pbuf, size_t buflen, const VALUE *pval, short dbfld_dbrtype, bool prefix_with_type = false)
 {
+    /* CHAR and UCHAR are typically used as SHORTSHORT,
+	 * so avoid mounting NULL-bytes into the string
+	 */
 	if (dbfld_dbrtype == DBFLD::D_CHAR) {
-       /* CHAR and UCHAR are typically used as SHORTSHORT,
-	    * so avoid mounting NULL-bytes into the string
-	    */
-        return epicsSnprintf(pbuf, buflen, "%s%d", (prefix_with_type ? "v_int8 " : ""), (int)pval->v_int8);
+		/* we print v_uint8 rather than v_int8, this is what caPutLog does */
+        return epicsSnprintf(pbuf, buflen, "%s%d", (prefix_with_type ? "v_int8 " : ""), (int)pval->v_uint8);
 	} else if (dbfld_dbrtype == DBFLD::D_UCHAR) {
         return epicsSnprintf(pbuf, buflen, "%s%d", (prefix_with_type ? "v_uint8 " : ""), (int)pval->v_uint8);
 	} else if (dbfld_dbrtype == DBFLD::D_SHORT) {
