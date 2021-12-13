2bede6ff75ac6d87da7b1ad4f9b330b8e2aafe2b
Ralph Lange <ralph.lange@gmx.de>
2021-12-13 12:41:58 +0100

    test: add EPICS_BASE binaries to PATH
    - needed to find caRepeater

--- testTop/pyTestsApp/gwtests.py
+++ testTop/pyTestsApp/gwtests.py
@@ -65,3 +65,8 @@ def setup():
         iocExecutable = os.path.join(os.environ['EPICS_BASE'], 'bin', hostArch, 'softIoc')
     else:
         print("Warning: IOC_EPICS_BASE or EPICS_BASE not set. Running 'softIoc' executable in PATH")
+
+    if 'EPICS_BASE' in os.environ:
+        os.environ['PATH'] += os.pathsep + os.path.join(os.environ['EPICS_BASE'], 'bin', hostArch)
+    else:
+        print("Warning: EPICS_BASE not set. Will look for 'caRepeater' executable in PATH")
